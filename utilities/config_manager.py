d155bbfc296e458db46f599839634e40eee1440f
tryiou <tryou@tryou.com>
2025-07-15 13:59:58 +0200

    fix: update xlite launch options for Windows

--- utilities/config_manager.py
+++ utilities/config_manager.py
@@ -123,7 +123,7 @@ class ConfigManager:
                 "Darwin": "XLite-1.0.7-mac"
             },
             'xlite_launch_options': {
-                "Windows": ["--in-process-gpu"],
+                "Windows": ["--disable-gpu"],
                 "Linux": [],
                 "Darwin": []
             },
